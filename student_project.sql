00ac1eb1d751bbb2462962ec158b727293ffa04b
Ramisetty Sri vasu <srivasuramisetty@gmail.com>
2026-05-02 09:29:31 +0530

    Remove students table creation from SQL script
    Removed the creation of the 'students' table from the SQL script.

--- student_project.sql
+++ student_project.sql
@@ -1,8 +1,3 @@
 create database student;
 use student;
-create table students(
-id int primary key auto_increment,
-name varchar(60)  not null,
-age int not null,
-login int not null);
 select * from student_1;
